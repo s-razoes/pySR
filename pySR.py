fb62d230d52ea166387bdec8b512a28084643059
s-razoes <49306008+s-razoes@users.noreply.github.com>
2020-09-10 23:34:00 +0100

    Small update
    Can accept certificate files locations as arguments
    Can respond a file instead of just a line

--- pySR.py
+++ pySR.py
@@ -6,11 +6,13 @@ from datetime import datetime
 import argparse
 
 parser = argparse.ArgumentParser(prog='redirect')
-parser.add_argument('-url', type=str, default='', help='Location to redirect to.')
+parser.add_argument('-location', type=str, default='', help='Location to redirect to.')
 parser.add_argument('-respond', type=str, default='', help='Body text to respond.')
+parser.add_argument('-respondF', type=str, default='', help='File to respond.')
 parser.add_argument('-p', type=int, default=8080, help='Port to serve [Default=8080]')
 parser.add_argument('-c', type=int, default=200, help='HTTP code to return [Default=200]')
-parser.add_argument('-ssl', action='store_true', help='Run SSL ("local.key", "local.crt")')
+parser.add_argument('--cert', type=str, default='', help='Location of certificate file to use as public key in SSL connections')
+parser.add_argument('--pKey', type=str, default='', help='Location of file to use as private key in SSL connections')
 parser.add_argument('-log', type=str, default='', help='Logfile prefix.')
 parser.add_argument('-hostname', type=str, default='', help='Hostname/ip address to respond to, by default respond to all.')
 
@@ -26,16 +28,31 @@ logger.setLevel(logging.DEBUG)
 log_format = '>%(asctime)s :\n%(message)s'
 logging.basicConfig(filename=logFileName,format=log_format, datefmt='%Y-%m-%d %H:%M:%S')
 
+#check uncompatible arguments
+if args.respond and args.respondF:
+    print('Can only have one response argument')
+    quit()
+
+#has all the conditions for the certificates
+if args.cert != '' or args.pKey != '':
+    if args.cert == '' or args.pKey =='':
+        print('When introducing certificates, both must be present')
+        quit()
 
 if args.respond:
     response = bytes(args.respond, 'UTF8')
+
+if args.respondF:
+    f = open(args.respondF, 'rb')
+    response = f.read()
+    f.close()
 
 class myHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         logger.info(self.headers)
         self.send_response(args.c)
-        if args.url:
-            self.send_header('Location',args.url)
+        if args.location:
+            self.send_header('Location',args.location)
         self.end_headers()
         if response:
             self.wfile.write(response)
@@ -48,17 +65,20 @@ try:
 
     txt='Running '+args.hostname+' on port '+str(args.p)+' returning code '+str(args.c)
     
-    if args.url:
-        txt=txt+' and location to '+args.url
+    if args.location:
+        txt=txt+' and location to '+args.location
 
-    if args.ssl:
+    if args.cert:
         pywebserver.socket = ssl.wrap_socket (pywebserver.socket, 
-            keyfile="local.key", 
-            certfile='local.crt', server_side=True)
+            keyfile=args.pKey, 
+            certfile=args.cert, server_side=True)
         txt=txt+' with SSL'
     
     if args.respond:
         txt=txt+' respond with >'+args.respond
+    
+    if args.respondF:
+        txt=txt+' respond with file '+args.respondF
     
     print(txt)
 
